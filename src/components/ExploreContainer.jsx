dad910b908496742fb7f2b0b10a92bd93c08e7df
aaadriana <adriana@stanciu.uk>
2021-11-15 12:48:23 +0000

    import React in all jsx files, import Firebase
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>

--- src/components/ExploreContainer.jsx
+++ src/components/ExploreContainer.jsx
@@ -1,21 +1,22 @@
-import "./ExploreContainer.css";
+import React from 'react';
+import './ExploreContainer.css';
 
 const ExploreContainer = ({ name }) => {
-	return (
-		<div className='container'>
-			<strong>{name}</strong>
-			<p>
-				Explore{" "}
-				<a
-					target='_blank'
-					rel='noopener noreferrer'
-					href='https://ionicframework.com/docs/components'
-				>
-					UI Components
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container">
+      <strong>{name}</strong>
+      <p>
+        Explore{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default ExploreContainer;
