9464f0f150008598d1b953739589cc95c769bb9a
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-11-25 17:25:21 +0000

    routine builder styling wip
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>

--- src/components/ProductListItem.jsx
+++ src/components/ProductListItem.jsx
@@ -1,12 +1,13 @@
 import { IonItem, IonLabel, IonIcon } from '@ionic/react';
 import { Draggable } from 'react-beautiful-dnd';
 import { reorderThreeOutline } from 'ionicons/icons';
+import './ProductListItem.css';
 
 const ProductListItem = ({ product, index }) => {
   return (
     <Draggable draggableId={product.id} index={index}>
       {(provided) => (
-        <IonItem {...provided.draggableProps} ref={provided.innerRef}>
+        <IonItem className="productListItem" {...provided.draggableProps} ref={provided.innerRef}>
           <IonLabel>{product.name}</IonLabel>
           <IonIcon icon={reorderThreeOutline} color="add-btn" {...provided.dragHandleProps} />
         </IonItem>
